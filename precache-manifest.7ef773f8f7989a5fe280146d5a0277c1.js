ea4f46a42a2bc27b69a65553672ed15f5b665621
sanyalu <sanyalu97@gmail.com>
2023-04-09 15:24:58 +0200

    Updates

---
+++ precache-manifest.7ef773f8f7989a5fe280146d5a0277c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d380b9dbf6e8011112484cc3d6da0d",
+    "url": "/todolist-incubator/index.html"
+  },
+  {
+    "revision": "45a180ff9a97c1ee6c57",
+    "url": "/todolist-incubator/static/css/main.0826f08b.chunk.css"
+  },
+  {
+    "revision": "3e31f0193fff9615c82d",
+    "url": "/todolist-incubator/static/js/2.be0ccc80.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist-incubator/static/js/2.be0ccc80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a180ff9a97c1ee6c57",
+    "url": "/todolist-incubator/static/js/main.1a9c01d4.chunk.js"
+  },
+  {
+    "revision": "562305b3e27188c2e3ec",
+    "url": "/todolist-incubator/static/js/runtime-main.8b98fea1.js"
+  }
+]);
